abcfca110e51b0e4fe715bc6a3bd717d47b87414
Brian Tomlinson <brian@smartm.no>
2014-03-06 20:56:04 +0100

    Fix the ever duplicating menu issue and fix the problem with the second open of the about dialog causing a crash due to a missing child.

--- usr/lib/python3/dist-packages/liteupdater/liteupdater.py
+++ usr/lib/python3/dist-packages/liteupdater/liteupdater.py
@@ -11,6 +11,9 @@ class SystrayApp(object):
     def __init__(self):
         self.tray = Gtk.StatusIcon()
         self.menu = Gtk.Menu()
+        self.menu_about = Gtk.MenuItem()
+        self.menu_quit = Gtk.MenuItem()
+        self.about_dialog = ''
 
         if os.path.isfile('/usr/share/pixmaps/updaten.png'):
             self.tray.set_from_file('/usr/share/pixmaps/updaten.png')
@@ -21,21 +24,24 @@ class SystrayApp(object):
         self.tray.connect('popup-menu', self.on_right_click)
 
     def on_right_click(self, icon, event_button, event_time):
-        print("Right-click fired")
         self.make_menu(icon, event_button, event_time)
 
     def make_menu(self, icon, event_button, event_time):
 
-        about = Gtk.MenuItem()
-        about.set_label("About")
-        about.connect('activate', self.show_about_dialog)
+        try:
+            for widget in self.menu.get_children():
+                self.menu.remove(widget)
+        except Exception as e:
+            print(e)
 
-        quit_action = Gtk.MenuItem()
-        quit_action.set_label("Quit")
-        quit_action.connect("activate", Gtk.main_quit)
+        self.menu_about.set_label("About")
+        self.menu_about.connect('activate', self.show_about_dialog)
 
-        self.menu.append(about)
-        self.menu.append(quit_action)
+        self.menu_quit.set_label("Quit")
+        self.menu_quit.connect("activate", Gtk.main_quit)
+
+        self.menu.append(self.menu_about)
+        self.menu.append(self.menu_quit)
 
         self.menu.show_all()
 
@@ -43,18 +49,17 @@ class SystrayApp(object):
             return Gtk.StatusIcon.position_menu(menu, icon)
 
         self.menu.popup(None, None, pos, icon, event_button, event_time)
-        print("Menu should have popped up")
 
     def show_about_dialog(self, widget):
-        about_dialog = Gtk.AboutDialog()
-        about_dialog.set_destroy_with_parent(True)
-        about_dialog.set_icon_name('Lite Updater')
-        about_dialog.set_name('Lite Updater')
-        about_dialog.set_version('0.1')
-        about_dialog.set_comments(u'Check for Linux Lite updates')
-        about_dialog.set_authors([u'Brian Tomlinson <brian.tomlinson@linux.com>'])
-        about_dialog.run()
-        about_dialog.destroy()
+        self.about_dialog = Gtk.AboutDialog()
+        self.about_dialog.set_destroy_with_parent(True)
+        self.about_dialog.set_icon_name('Lite Updater')
+        self.about_dialog.set_name('Lite Updater')
+        self.about_dialog.set_version('0.1')
+        self.about_dialog.set_comments(u'Check for Linux Lite updates')
+        self.about_dialog.set_authors([u'Brian Tomlinson <brian.tomlinson@linux.com>'])
+        self.about_dialog.run()
+        self.about_dialog.destroy()
 
     def show_log(self, widget):
         subprocess.Popen('/usr/bin/leafpad /tmp/liteupdater.log', shell=True)
